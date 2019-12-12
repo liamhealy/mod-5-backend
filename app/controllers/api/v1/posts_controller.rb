2a0fb2cbccc03093de5e520e0c6dc309fc41edf4
Liam <liam.healy1@marist.edu>
2019-12-12 15:14:02 -0500

    Set up post routes, controller and some validations

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,60 @@
 class Api::V1::PostsController < ApplicationController
+
+    def index
+        render json: { posts: Post.all}
+    end
+
+    def show
+        render json: { post: Post.find_by(id: params[:id]) }
+    end
+
+    def create
+        post = Post.new(create_params[:post])
+        if post.save
+            render json: { post: post }
+        else
+            render json: { error: "Something went wrong..." }
+        end
+    end
+
+    def update
+        post = Post.find_by(id: params[:id])
+        if post.update(update_params[:post])
+            render json: { post: post }
+        else
+            render json: { error: "Something went wrong..." }
+        end
+    end
+
+    def destroy
+        post = Post.find_by(id: params[:id])
+        puts "Destroying '#{post.title}', by #{post.user.username}"
+        if post.destroy
+            render json: { message: "Post has been removed." }
+        else
+            render json: { error: "Something went wrong..." }
+        end
+    end
+
+    # ----------------------------------
+    #-----------------------------------
+    private #---------------------------
+    #-----------------------------------
+    # ----------------------------------
+
+    def create_params
+        params.permit(
+            post: [
+                :user_id, :title, :description, :body
+            ]
+        )
+    end
+
+    def update_params
+        params.permit(
+            post: [
+                :id, :user_id, :title, :description, :body
+            ]
+        )
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :title, length: { minimum: 2 }
+    validates :description, presence: true
+    validates :description, length: { minimum: 3 }
+    validates :body, presence: true
+    validates :body, length: { minimum: 3 }
 end
