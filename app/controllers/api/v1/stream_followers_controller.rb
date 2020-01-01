473f8a7c88851fad4b58836e42be891a009584ca
Liam <liam.healy1@marist.edu>
2019-12-31 20:44:41 -0500

    Stream_follower stuff

--- app/controllers/api/v1/stream_followers_controller.rb
+++ app/controllers/api/v1/stream_followers_controller.rb
@@ -1,8 +1,50 @@
 class Api::V1::StreamFollowersController < ApplicationController
 
     def show
+        user = User.find_by(id: params[:id])
+        # stream_follower = StreamFollower.find_by(id: params[:id])
+        # options = {
+        #     include: [:user, :streamer]
+        # }
+        # render json: StreamFollowerSerializer.new(stream_follower, options)
+        render json: {following: user.stream_followers}
+    end
+
+    def create
+        # streamer = Streamer.find_or_create_by(name: params[:streamer_name])
+        # streamer = Streamer.find_or_create_by(id: params[:streamer_id])
+        streamer = Streamer.find_by(name: params[:streamer_name])
+        # byebug
+        params[:stream_follower][:streamer_id] = streamer.id
+        stream_follower = StreamFollower.create(create_params[:stream_follower])
+        if stream_follower.save
+            render json: {stream_follower: stream_follower}
+        else
+            render json: {error: "Something went wrong"}
+        end
+    end
+
+    def destroy
+        # user = User.find_by(id: params[:user_id])
+        # streamer = Streamer.find_by(name: params[:streamer_name])
+        # stream_follower = StreamFollower.find_by(user_id: user.id, streamer_id: streamer.id)
+        # stream_follower.destroy
         stream_follower = StreamFollower.find_by(id: params[:id])
-        render json: StreamFollowerSerializer.new(stream_follower)
+        if stream_follower.destroy
+            render json: {success: "You are no longer following this streamer."}
+        else
+            render json: {error: "Something went wrong."}
+        end
+    end
+
+    private
+
+    def create_params
+        params.permit(
+            stream_follower: [
+                :user_id, :streamer_id
+            ]
+        )
     end
 
 end
--- app/controllers/api/v1/streamers_controller.rb
+++ app/controllers/api/v1/streamers_controller.rb
@@ -1,11 +1,34 @@
 class Api::V1::StreamersController < ApplicationController
 
     def show
-        streamer = Streamer.find_by(name: params[:id])
+        streamer = Streamer.find_or_create_by(name: params[:id])
         options = {
             include: [:stream_followers]
         }
-        render json: StreamerSerializer.new(streamer, options)
+        if streamer
+            render json: StreamerSerializer.new(streamer, options)
+        else
+            render json: {error: "No user found."}
+        end
+    end
+
+    def create
+        streamer = Streamer.new(create_params[:streamer])
+        if streamer.save
+            render json: StreamerSerializer.new(streamer)
+        else
+            render json: {error: "Something went wrong..."}
+        end
+    end
+
+    private
+
+    def create_params
+        params.permit(
+            streamer: [
+                :name
+            ]
+        )
     end
 
 end
